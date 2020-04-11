5ed674ddfda8204393892f30c12fb2905d3a9e7d
WADAAKIRA1210 <wadakei1210.com>
2020-04-11 22:53:45 +0900

    訂正部分の追加

--- EX1_3_1.cpp
+++ EX1_3_1.cpp
@@ -10,8 +10,18 @@
 using namespace std;
 
 int main() {
-	cout<<"input working time";
-	
-	
+	int WorkTime;
+	int HourlyWage;
+	int Earn;
+	cout<<"input working time"<<endl;
+	cin>>WorkTime;
+
+	cout<<"input hourly wage"<<endl;
+	cin>>HourlyWage;
+
+	Earn = WorkTime * HourlyWage;
+
+	cout << "sum earn is " << Earn<<endl;
+
 	return 0;
 }
